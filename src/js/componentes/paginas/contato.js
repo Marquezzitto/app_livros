8f19d3634a9a7e6e120675183f4410c2727fdf1f
Marquezzitto <marcoshmarques@gmail.comm>
2026-06-27 08:38:24 -0300

    feat: padronização das paginas

--- src/js/componentes/paginas/contato.js
+++
@@ -1,5 +0,0 @@
-const contato = `<h1> Esta é a Página De Contato </h1> <p> Entre em contato conosco pelo Email:</p>`
-
-
-
-export default contato;
--- src/js/componentes/paginas/home.js
+++
@@ -1,5 +0,0 @@
-const home = `<h1> Esta é a Página Inicial </h1> <p> Bem Vindo Ao Site</p>`
-
-
-
-export default home;
